be0be30a8b5b0054d6e384925c1f2b0e5421bb1e
Felipe Maion <felipe.maion@gmail.com>
2020-03-30 00:30:10 -0300

    Frontend React Init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Logon from './pages/Logon';
+// JSX 
 
+// COMPOMENTE:
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Here I am... rocking like a hurricane!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+  <Logon>BE THE HERO</Logon>
   );
 }
 
---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css'
+
+export default function Logon(){
+    return(
+        <div>
+        <h1>HELLO!</h1>
+        <a href="https://youtu.be/jFl90T6lnBQ?t=1841">Parei aqui.</a> 
+        </div>
+    );
+}
